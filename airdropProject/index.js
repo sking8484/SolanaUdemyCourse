d61985398083871d33d68596a1d376518d6e6dcd
Darst King <darstking@Darsts-MacBook-Pro.local>
2022-04-05 17:59:07 -0700

    Got Private / public keys, created index.js

---
+++ airdropProject/index.js
@@ -0,0 +1,15 @@
+const {
+    Connection,
+    Publickey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL
+} = require("@solana/web3.js")
+
+const wallet = new Keypair()
+
+const publicKey = wallet._keypair.publicKey 
+const secretKey = wallet._keypair.secretKey
+
+console.log(publicKey)
+console.log(secretKey)
