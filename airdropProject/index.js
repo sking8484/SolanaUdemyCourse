f631f5e5ec65cd53aa6c13c2c89f809396529b9d
Darst King <darstking@Darsts-MacBook-Pro.local>
2022-04-06 21:29:55 -0700

    Get wallet Balance code

--- airdropProject/index.js
+++ airdropProject/index.js
@@ -11,3 +11,17 @@ const wallet = new Keypair()
 const publicKey = new PublicKey(wallet._keypair.publicKey) 
 const secretKey = wallet._keypair.secretKey
 
+const getWalletBalance = async() => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'),"confirmed")
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log(`Wallet balance is ${walletBalance}`)
+    } catch(err){
+        console.error(err) 
+    }
+}
+
+const main = async() => {
+    await getWalletBalance();
+}
+main()
